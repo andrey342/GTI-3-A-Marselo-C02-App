d161855e2848ef2892433cb2b06342d5d59d1337
Andrey <akuzmin@epsg.upv.es>
2021-09-17 15:25:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gti_3a_kuzminandrey_sprint0/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gti_3a_kuzminandrey_sprint0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
